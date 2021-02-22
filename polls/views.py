8c6842e05e66ef8be279ef97d13fe320c91cad49
Cordamena <codramena@gmail.com>
2021-02-22 19:56:38 +0700

    commit5

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>With great power comes great responsobility<h1>")
+
+def meme(request):
+    return HttpResponse("<img src='https://management30.com/wp-content/uploads/2016/03/change-management-dead.jpg'>")
