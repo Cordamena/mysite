27c87061e871cc79f788637a85ba55a00ed2740a
Cordamena <codramena@gmail.com>
2021-02-27 19:54:20 +0700

    anotherday

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Модель для обработки вопроса
+class Question(models.Model):
+    text = models.CharField(max_length=1000)
+    pub_Date = models.DateTimeField("date published")
+    #в момент конверации в str()
+    def __str__(self):
+        return self.text
+class Choice(models.Model):
+    # связываем question и choice
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    votes = models.IntegerField(default=0)
+    #В момент конвертации в str()
+    def __str__(self):
+        return self.text
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+from .models import Question, Choice
+
+
 def index(request):
-    return HttpResponse("<h1>With great power comes great responsobility<h1>")
+    q_all = Question.objects.all()
+    res = "<ol>"
+    for g in q_all:
+        res += "<li>%s</li>" % g.text
+    res += "</ol>"
+
+    return HttpResponse(res)
 
 def meme(request):
     return HttpResponse("<img src='https://management30.com/wp-content/uploads/2016/03/change-management-dead.jpg'>")
+
+def detail(request, q_id):
+    res = "Question number %s." % q_id
+    return HttpResponse(res)
+
+def results(request, q_id):
+    res = "Result fot Question number %s." % q_id
+    return HttpResponse(res)
+
+def vote(request, q_id):
+    res = "Vote fot Question number %s." % q_id
+    return HttpResponse(res)
