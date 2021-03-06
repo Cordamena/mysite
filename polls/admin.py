b739c245240fb26b6879da93719a0f7c714a0e1c
Cordamena <codramena@gmail.com>
2021-03-06 19:52:19 +0700

    flaf my life

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceAdmin(admin.StackedInline):
+    fieldsets = [
+        ("Вариант ответа", {"fields" : ["text", "votes"]})
+    ]
+
+    model = Choice
+    extra = 3
+
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Text of the question", {"fields" : ["text"]}),
+        ("Date", {"fields" : ["pub_Date"]})
+    ]
+    inlines = [ChoiceAdmin]
+    list_filter = ["pub_Date"]
+    search_fields = ["text"]
+
+admin.site.register(Question, QuestionAdmin)
 
--- polls/views.py
+++ polls/views.py
@@ -6,20 +6,23 @@ from .models import Question, Choice
 
 
 def index(request):
-    q_all = Question.objects.all()
-    res = "<ol>"
-    for g in q_all:
-        res += "<li>%s</li>" % g.text
-    res += "</ol>"
+    questions = Question.objects.all()
 
-    return HttpResponse(res)
+    context = {
+        "questions" : questions
+    }
+    return render(request, "polls/index.html", context)
 
 def meme(request):
     return HttpResponse("<img src='https://management30.com/wp-content/uploads/2016/03/change-management-dead.jpg'>")
 
 def detail(request, q_id):
-    res = "Question number %s." % q_id
-    return HttpResponse(res)
+    question = Question.objects.get(pk=q_id)
+    # choices = question.choice_set.all()
+    context = {
+        "question" : question
+    }
+    return render(request, "polls/detail.html", context )
 
 def results(request, q_id):
     res = "Result fot Question number %s." % q_id
